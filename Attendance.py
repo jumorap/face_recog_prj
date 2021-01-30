f98d06bbfbef9432741c8088219c6fa56503446d
Jumorap <jumorap@unal.edu.co>
2021-01-29 21:56:48 -0500

    Now, the users that don't have their face into "Images" file, will be added with a specific code

--- Attendance.py
+++ Attendance.py
@@ -8,22 +8,28 @@ import pyttsx3
 from email.mime.multipart import MIMEMultipart
 from email.mime.image import MIMEImage
 
-path = 'Images'
-images = []
-class_names = []
-myList = os.listdir(path)
+# This will be a simple variable that save names that was returned by [say_hello]
 after_detect = []
 
-print(myList)
 
-# We are just generating the image name to after use it in
-# face recognition only adding a photo to root 'images'
-for cl in myList:
-    cur_img = cv2.imread(f'{path}/{cl}')
-    images.append(cur_img)
-    class_names.append(os.path.splitext(cl)[0])
+def cover_files_lunch():
+    path = 'Images'
+    images = []
+    class_names = []
+    my_list = os.listdir(path)
 
-print(class_names)
+    print(my_list)
+
+    # We are just generating the image name to after use it in
+    # face recognition only adding a photo to root 'images'
+    for cl in my_list:
+        cur_img = cv2.imread(f'{path}/{cl}')
+        images.append(cur_img)
+        class_names.append(os.path.splitext(cl)[0])
+
+    print(class_names)
+
+    start(images, class_names)
 
 
 def find_encodings(images):
@@ -91,14 +97,13 @@ def say_hello(name, img):
     # and is called the def [send_mail_record], where we gonna advise who is in camera.
     # Additional to it, the machine say a message when recognize the face of somebody
     if name not in after_detect:
-
         print(name)
 
         # Generate one string concatenated with [name] and one integer that define
         # the voice rate or voice speed.
         # After, using pyttsx3 library, we start the 'reader', and is saved in [player]
         # to after send [say_my_name] to be reader with the speed defined in [new_voice_rate]
-        say_my_name = f'¡Hola! ¿Cómo estás {name}?'
+        say_my_name = f'Bienvenido {name}'
         new_voice_rate = 145
 
         player = pyttsx3.init()
@@ -107,41 +112,56 @@ def say_hello(name, img):
         player.runAndWait()
 
         after_detect.append(name)
-        send_mail_record(name, img)
+        try:
+            send_mail_record(name, img)
+        except:
+            print("ERROR: Chek the session")
         cv2.imshow("Somebody", img)
 
 
-encode_list_known_faces = find_encodings(images)
+def start(images, class_names):
+    encode_list_known_faces = find_encodings(images)
+    cap = cv2.VideoCapture(0)
 
-print('Complete')
+    print('Complete')
 
-cap = cv2.VideoCapture(0)
+    while True:
+        # We do that the system is "listening" constantly the camera
+        # who was opened before with [cap]
+        success, img = cap.read()
+        img_small = cv2.resize(img, (0, 0), None, 0.25, 0.25)
+        img_small = cv2.cvtColor(img_small, cv2.COLOR_BGR2RGB)
+        color_box = (0, 255, 0)
 
-while True:
-    # We do that the system is "listening" constantly the camera
-    # who was opened before with [cap]
-    success, img = cap.read()
-    img_small = cv2.resize(img, (0, 0), None, 0.25, 0.25)
-    img_small = cv2.cvtColor(img_small, cv2.COLOR_BGR2RGB)
+        # First, we trace and calculate the distance of the image
+        face_current_frame = face_recognition.face_locations(img_small)
+        encode_face_current_frame = face_recognition.face_encodings(img_small, face_current_frame)
 
-    # First, we trace and calculate the distance of the image
-    face_current_frame = face_recognition.face_locations(img_small)
-    encode_face_current_frame = face_recognition.face_encodings(img_small, face_current_frame)
+        for encode_face, face_loc in zip(encode_face_current_frame, face_current_frame):
+            match_face = face_recognition.compare_faces(encode_list_known_faces, encode_face)
+            face_dista = face_recognition.face_distance(encode_list_known_faces, encode_face)
 
-    for encode_face, face_loc in zip(encode_face_current_frame, face_current_frame):
-        match_face = face_recognition.compare_faces(encode_list_known_faces, encode_face)
-        face_dista = face_recognition.face_distance(encode_list_known_faces, encode_face)
+            match_index = np.argmin(face_dista)
 
-        match_index = np.argmin(face_dista)
+            y1, x2, y2, x1 = face_loc
+            y1, x2, y2, x1 = y1 * 4, x2 * 4, y2 * 4, x1 * 4
+            name = "UNKNOWN"
 
-        y1, x2, y2, x1 = face_loc
-        y1, x2, y2, x1 = y1 * 4, x2 * 4, y2 * 4, x1 * 4
-        name = "UNKNOWN"
-        if match_face[match_index]:
-            # When the conditions is met, is select the name of person who
-            # appear in camera if they are registered on file "images" with
-            # their respective name
-            name = class_names[int(match_index)].upper()
+            if match_face[match_index]:
+                # When the conditions is met, is select the name of person who
+                # appear in camera if they are registered on file "images" with
+                # their respective name
+                name = class_names[int(match_index)].upper()
+                color_box = (0, 255, 0)
+            else:
+                # If the face don't match with the models from "Images", will be
+                # saved with an identifier *%d%m%y%H%M%S* and after will be
+                # added to "Images" set
+                color_box = (0, 0, 255)
+                timer = time.strftime("%d%m%y%H%M%S")
+                face_img = "Images/%d.jpg" % int(timer)
+                cv2.imwrite(face_img, img)
+                break
 
             # Is generated a box where tanks to library 'face_recognition', we
             # trace the face of our users, and is multiplied for 4, because
@@ -151,19 +171,20 @@ while True:
             #
             # Both within, we define the color of border and background of our
             # boxes, too is defined the font, their size and color
-            cv2.rectangle(img, (x1, y1 - 30), (x2, y2 + 20), (0, 255, 0), 1)
-            cv2.rectangle(img, (x1, y2 - 15), (x2, y2 + 20), (0, 255, 0), cv2.FILLED)
+            cv2.rectangle(img, (x1, y1 - 30), (x2, y2 + 20), color_box, 1)
+            cv2.rectangle(img, (x1, y2 - 15), (x2, y2 + 20), color_box, cv2.FILLED)
             cv2.putText(img, name, (x1 + 5, y2 + 10), cv2.FONT_ITALIC, 0.7, (255, 255, 255), 2)
-
-            say_hello(name, img)
-        else:
-            cv2.rectangle(img, (x1, y1 - 30), (x2, y2 + 20), (0, 0, 255), 1)
-            cv2.rectangle(img, (x1, y2 - 15), (x2, y2 + 20), (0, 0, 255), cv2.FILLED)
-            cv2.putText(img, name, (x1 + 5, y2 + 10), cv2.FONT_ITALIC, 0.7, (255, 255, 255), 2)
-
             say_hello(name, img)
 
-    # With cv2.imshow we open a window where look the "camera process"
-    # and the name of who appear in camera
-    cv2.imshow('Webcam', img)
-    cv2.waitKey(1)
+        # With cv2.imshow we open a window where look the "camera process"
+        # and the name of who appear in camera
+        cv2.imshow('Webcam', img)
+        cv2.waitKey(1)
+
+        if color_box == (0, 0, 255):
+            break
+
+    cover_files_lunch()
+
+
+cover_files_lunch()
